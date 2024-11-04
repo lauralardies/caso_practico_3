e9d986c251249d5c8e62f068f532c3d8770a832f
lauralardies <lrodrlar@myuax.com>
2024-11-04 15:19:10 +0100

    datos codigo

---
+++ datos.py
@@ -0,0 +1,10 @@
+import yfinance as yf
+from datetime import datetime, timedelta
+
+# Define el símbolo del ticker y el periodo de tiempo
+tiker = '^IBEX'
+start_date = '2012-01-01'
+end_date = datetime.now().strftime('%Y-%m-%d')
+
+# Obtener los datos de las acciones
+data = yf.download(tiker, start=start_date, end=end_date)
